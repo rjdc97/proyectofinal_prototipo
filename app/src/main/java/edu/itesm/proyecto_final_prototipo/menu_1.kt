02c84819b482103b46fcbd31a493c5c39a62230f
Pedro Ramos <otinebordep@gmail.com>
2021-03-17 23:33:35 -0600

    Initial commit

---
+++ app/src/main/java/edu/itesm/proyecto_final_prototipo/menu_1.kt
@@ -0,0 +1,21 @@
+package edu.itesm.proyecto_final_prototipo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class menu_1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_1)
+
+        Handler().postDelayed({
+        startActivity(Intent(this,MainActivity::class.java))
+
+        // close this activity
+        finish()
+    }, 3000)
+
+    }
+}
